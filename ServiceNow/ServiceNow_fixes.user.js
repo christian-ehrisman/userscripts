4f56e5a802836a6d758e50b67cbe33e3eaf06492
Christian Ehrisman <christian.ehrisman@gmail.com>
2026-02-09 07:56:55 -0500

    Update version number to 1.6 and add download URL in ServiceNow Fixes script

--- ServiceNow/ServiceNow_fixes.user.js
+++ ServiceNow/ServiceNow_fixes.user.js
@@ -2,9 +2,10 @@
 // ==UserScript==
 // @name     ServiceNow Fixes
 // @description Functionally equivalent to clicking the "Start" button on the time worked button right before hitting save/submit in ServiceNow. Does not interfere with other use of the time tracking fields
-// @version 1.5
+// @version 1.6
 // @match  https://*.service-now.com/*
 // @grant    GM_addStyle
+// @downloadURL https://github.com/christian-ehrisman/userscripts/raw/refs/heads/master/ServiceNow/ServiceNow_fixes.user.js
 // ==/UserScript==
 
 // Get current time_worked value
